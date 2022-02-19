d5911302fee8d398b96bbfd01ce7785d6bcd18c2
jurbx <drimskiy.daniki@gmail.com>
2022-02-19 03:47:30 +0200

    1

--- contact/admin.py
+++ contact/admin.py
@@ -1,6 +1,16 @@
 from django.contrib import admin
-from .models import Contact, OurShops
+from .models import Contact, OurShops, TitleOurShop
+
+
+class OurShopsInline(admin.StackedInline):
+    model = OurShops
+    extra = 5
 
 
 admin.site.register(Contact)
-admin.site.register(OurShops)
+
+
+@admin.register(TitleOurShop)
+class MainThingAdmin(admin.ModelAdmin):
+    list_display = ['title', 'desc']
+    inlines = [OurShopsInline]
--- contact/models.py
+++ contact/models.py
@@ -11,7 +11,18 @@ class Contact(models.Model):
         verbose_name = 'Контакти'
 
 
+class TitleOurShop(models.Model):
+    title = models.CharField(max_length=200)
+    desc = models.TextField(max_length=500)\
+
+
+    class Meta:
+        verbose_name_plural = 'Наші магазини'
+        verbose_name = 'Наш магазин'
+
+
 class OurShops(models.Model):
+    title = models.ForeignKey(TitleOurShop, on_delete=models.CASCADE)
     city = models.CharField(max_length=100, verbose_name='Назва міста')
     info = models.TextField(max_length=1000, verbose_name='Інформація про постачальників')
 
