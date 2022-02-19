5551af241830a6326e71cd2b8ca1b6ec95f207c1
jurbx <74430098+jurbx@users.noreply.github.com>
2022-02-19 17:57:48 +0200

    Update urls.py

--- cart/urls.py
+++ cart/urls.py
@@ -8,5 +8,5 @@ urlpatterns = [
     path('detail/<slug:slug>/', model_view, name='model_view'),
     path('create/', CreateModelView.as_view(), name='model_create'),
     path('edit/<slug:slug>/', login_required(EditModelView.as_view()), name='model_edit'),
-    path('detail/<slug:slug>/3d/', three_d_model, name='three_d_model')
+    path('3d/detail/<slug:slug>/', three_d_model, name='three_d_model')
 ]
