6cf75b7cf3394638b96a89e3793fdea034ca7e4e
jurbx <drimskiy.daniki@gmail.com>
2022-02-22 01:37:41 +0200

    paginator in menu

--- cart/urls.py
+++ cart/urls.py
@@ -1,12 +1,10 @@
 from django.urls import path
-from .views import cart_view, model_view, CreateModelView, EditModelView, three_d_model
+from .views import cart_view, model_view, three_d_model
 from django.contrib.auth.decorators import login_required
 
 
 urlpatterns = [
     path('', cart_view, name='cart_view'),
     path('detail/<slug:slug>/', model_view, name='model_view'),
-    path('create/', CreateModelView.as_view(), name='model_create'),
-    path('edit/<slug:slug>/', login_required(EditModelView.as_view()), name='model_edit'),
-    path('3d/detail/<slug:slug>/', three_d_model, name='three_d_model')
+    path('3d/detail/<slug:slug>/', three_d_model, name='three_d_model'),
 ]
--- cart/views.py
+++ cart/views.py
@@ -1,3 +1,4 @@
+from django.core.paginator import Paginator
 from django.db.models import Max
 from django.http import HttpResponseRedirect
 from django.shortcuts import render
@@ -6,7 +7,6 @@ from django.utils.text import slugify
 
 from .models import SofaModel, CartImages, SofaModels, SofaTypes, Cart3dModels
 import random
-from .forms import SofaCreateForm, SofaEditForm, ImageForm, FbxForm
 from django.views import generic
 
 
@@ -36,6 +36,10 @@ def cart_view(request):
     if cur_type := request.GET.getlist('type'):
         sofas = sofas.filter(sofa_type__name__in=cur_type)
 
+    paginator = Paginator(sofas, 9)
+    page = request.GET.get('page')
+    sofas = paginator.get_page(page)
+
     categories = SofaModels.objects.all()
     types = SofaTypes.objects.all()
     return render(request, 'cart_detail_2.html', context={'sofas': sofas,
@@ -58,65 +62,3 @@ def model_view(request, slug):
                                                                 'img': img,
                                                                 'more_sofa': more_sofa,
                                                                 'fbx': fbx})
-
-
-class CreateModelView(generic.CreateView):
-    model = SofaModel
-    template_name = 'shop_product_create.html'
-    success_url = reverse_lazy('cart_view')
-    fields = '__all__'
-
-    def get_context_data(self, **kwargs):
-        context = super().get_context_data(**kwargs)
-        context['image_form'] = ImageForm()
-        context['fbx_form'] = FbxForm()
-        return context
-
-    def form_valid(self, form):
-        self.object = form.save()
-        self.object.slug = slugify(self.object.title)
-        self.object.save()
-
-        fbx_form = FbxForm(self.request.POST, self.request.FILES, instance=self.object)
-        if fbx_form.is_valid():
-            fbx_form.save()
-
-        image_form = ImageForm(self.request.POST, self.request.FILES, instance=self.object)
-        if image_form.is_valid():
-            image_form.save()
-        return HttpResponseRedirect(self.get_success_url())
-
-
-class EditModelView(generic.UpdateView):
-    model = SofaModel
-    template_name = 'shop_product_edit.html'
-    success_url = reverse_lazy('model_view')
-    form_class = SofaEditForm
-
-    def get_success_url(self):
-        return reverse('model_view', kwargs={'slug': self.object.slug})
-
-    def get_context_data(self, **kwargs):
-        context = super().get_context_data(**kwargs)
-        if self.request.method == 'POST':
-            context['image_form'] = ImageForm(self.request.POST, self.request.FILES, instance=self.object)
-            context['fbx_form'] = FbxForm(self.request.POST, self.request.FILES, instance=self.object)
-        else:
-            context['image_form'] = ImageForm(instance=self.object)
-            context['fbx_form'] = FbxForm(instance=self.object)
-        return context
-
-    def form_valid(self, form):
-        context = self.get_context_data()
-        image_form = context['image_form']
-        fbx_form = context['fbx_form']
-
-        self.object = form.save()
-        self.object.slug = slugify(self.object.title)
-
-        if fbx_form.is_valid():
-            fbx_form.save()
-
-        if image_form.is_valid():
-            image_form.save()
-        return super(EditModelView, self).form_valid(form)
