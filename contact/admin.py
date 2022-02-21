63baca703832f4b61dff1eb185ce28b5e27c4e8d
jurbx <drimskiy.daniki@gmail.com>
2022-02-21 19:17:43 +0200

    update models documentation

--- contact/admin.py
+++ contact/admin.py
@@ -2,16 +2,7 @@ from django.contrib import admin
 from .models import Contact, OurShops, TitleOurShop
 
 
-class OurShopsInline(admin.StackedInline):
-    model = OurShops
-    extra = 5
-
-
 admin.site.register(Contact)
 admin.site.register(OurShops)
+admin.site.register(TitleOurShop)
 
-
-@admin.register(TitleOurShop)
-class MainThingAdmin(admin.ModelAdmin):
-    list_display = ['title', 'desc']
-    inlines = [OurShopsInline]
