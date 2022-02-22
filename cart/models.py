af07675a0574552fb2325e2ceedc297db09fd3be
jurbx <drimskiy.daniki@gmail.com>
2022-02-22 15:41:49 +0200

    update main menu

--- cart/models.py
+++ cart/models.py
@@ -36,7 +36,7 @@ class SofaModel(models.Model):
     sofa_type = models.ForeignKey(SofaTypes, on_delete=models.CASCADE, verbose_name='Тип')
     priority = models.PositiveIntegerField(verbose_name='Приоритет показу диванів в каталозі', default=1)
     new = models.BooleanField(default=False, verbose_name='Новинка')
-    show_in_main_page = models.BooleanField(default=True)
+    show_in_main_page = models.BooleanField(default=True, verbose_name='Показувати на головній сторінці')
 
     class Meta:
         verbose_name_plural = 'Дивани'
