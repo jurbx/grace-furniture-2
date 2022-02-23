e388c4220b876ddc8e74e1d4e7dca3c31e826c73
jurbx <drimskiy.daniki@gmail.com>
2022-02-23 12:27:49 +0200

    english version

--- english/admin.py
+++ english/admin.py
@@ -2,5 +2,5 @@ from django.contrib import admin
 from .models import HeroSectionEn, AboutUsEn
 
 
-admin.site.regisster(HeroSectionEn)
-admin.site.regisster(AboutUsEn)
+admin.site.register(HeroSectionEn)
+admin.site.register(AboutUsEn)
