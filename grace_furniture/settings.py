20a4d66829cf132e336f4e25d4edfd7972244edd
jurbx <74430098+jurbx@users.noreply.github.com>
2023-05-11 19:53:51 +0200

    Update settings.py

--- grace_furniture/settings.py
+++ grace_furniture/settings.py
@@ -108,7 +108,7 @@ DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql_psycopg2',
         'NAME': DB_NAME,
-        'USER': DB_USER
+        'USER': DB_USER,
         'PASSWORD': DB_PASSWORD,
         'HOST': DB_HOST,
         'PORT': '',
